a5342a24c07d7f3e7ff5cc680ad4c926139d6f35
eli knaap <ek@knaaptime.com>
2018-12-20 11:22:37 -0800

    bring tests back

--- osnap/tests/test_importers.py
+++ osnap/tests/test_importers.py
@@ -1,15 +1,14 @@
-import os
 from context import data
-from zipfile import ZipFile
+import os
 
+path = os.environ['DLPATH']
 read_ltdb = data.read_ltdb
 
 
 def test_read_ltdb():
 
-    state = "AL"
     df = read_ltdb(
-        sample=os.path.join(os.getcwd(), "ltdb_test.zip"),
-        fullcount=os.path.join(os.getcwd(), "ltdb_test.zip"),
+        sample=path+"/ltdb_sample.zip",
+        fullcount=path+"/ltdb_full.zip",
     )
-    assert df.state[1] == state
+    assert df.shape == (330388, 192)
