670968a8755ec0439004f50f46a303f335294690
eli knaap <ek@knaaptime.com>
2019-01-11 10:08:42 -0800

    fix ltdb filepath in tests

--- osnap/tests/test_1_importers.py
+++ osnap/tests/test_1_importers.py
@@ -7,6 +7,7 @@ read_ltdb = data.read_ltdb
 
 _package_directory = os.path.dirname(os.path.abspath(__file__))
 
+
 def test_read_ltdb():
 
     read_ltdb(
@@ -15,6 +16,6 @@ def test_read_ltdb():
     )
 
     df = pd.read_parquet(os.path.join(
-        _package_directory, "ltdb.parquet.gzip"))
+        _package_directory, "../data/ltdb.parquet.gzip"))
     df = data.db.ltdb
     assert df.shape == (330388, 192)
