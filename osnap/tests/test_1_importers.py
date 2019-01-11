eaa64b6854256f47bc948ffe044ba3174bd1b442
eli knaap <ek@knaaptime.com>
2019-01-11 09:38:38 -0800

    update importer test

--- osnap/tests/test_1_importers.py
+++ osnap/tests/test_1_importers.py
@@ -1,9 +1,11 @@
 from context import data
 import os
+import pandas as pd
 
 path = os.environ['DLPATH']
 read_ltdb = data.read_ltdb
 
+_package_directory = os.path.dirname(os.path.abspath(__file__))
 
 def test_read_ltdb():
 
@@ -11,5 +13,8 @@ def test_read_ltdb():
         sample=path+"/ltdb_sample.zip",
         fullcount=path+"/ltdb_full.zip",
     )
+
+    df = pd.read_parquet(os.path.join(
+        _package_directory, "ltdb.parquet.gzip"))
     df = data.db.ltdb
     assert df.shape == (330388, 192)
