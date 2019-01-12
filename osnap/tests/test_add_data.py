45083d46941d8eb785b6033f8053312cd151639e
eli knaap <ek@knaaptime.com>
2019-01-11 17:22:28 -0800

    test for parquet creation in ltdb reader

--- osnap/tests/test_add_data.py
+++ osnap/tests/test_add_data.py
@@ -1,13 +1,18 @@
 import context
 import os
+import pytest
 
 path = os.environ['DLPATH']
 read_ltdb = context.data.read_ltdb
 
+osnap_pth = os.path.abspath(context.data.__file__)
 
+#@pytest.mark.run(order=1)
 def test_read_ltdb():
 
     read_ltdb(
         sample=path+"/ltdb_sample.zip",
         fullcount=path+"/ltdb_full.zip",
     )
+
+    assert os.path.exists(os.path.join(os.path.dirname(os.path.abspath(context.data.__file__)), "ltdb.parquet.gzip"))
