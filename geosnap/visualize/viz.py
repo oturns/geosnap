1cb09de486a5ee00bcc7965e4178ae2980a29442
eli knaap <ek@knaaptime.com>
2019-06-17 14:28:45 -0700

    working variable explorer

--- geosnap/visualize/viz.py
+++ geosnap/visualize/viz.py
@@ -3,6 +3,8 @@ import os
 import dash
 import geopandas as gpd
 import dash_core_components as dcc
+import dash_html_components as html
+
 import mapclassify
 import palettable
 import json
@@ -15,48 +17,99 @@ mapbox_access_token = 'pk.eyJ1Ijoia25hYXB0aW1lIiwiYSI6ImlQeUJxazgifQ.35yYbOewGVV
 gdf = dc.tracts[dc.tracts.geoid.str.startswith('11')]
 gdf = gdf.merge(dc.census[dc.census.year == 2010], on='geoid')
 
-gdf = gdf.assign(
-    cl=mapclassify.Equal_Interval(gdf.n_nonhisp_black_persons, k=6).yb)
+opts = [{'label': col.title(), 'value': col} for col in gdf.columns]
 
 precomputed_color_ranges = palettable.colorbrewer.sequential.Blues_6.hex_colors
 layers = []
-
-for i, lyr in enumerate(precomputed_color_ranges):
-    example = {
-        'name': 'Layer name',
-        'source': json.loads(gdf[gdf.cl == i].to_json()),
-        'sourcetype': 'geojson',
-        'type': 'fill',
-        'opacity': 1.0,
-        'color': lyr  # Dynamically replace this with your color options
-    }
-    layers.append(example)
 
 trace = dict(type='scattermapbox',
              autocolorscale=True,
              locations=dc.tracts['geoid'],
              name='DC',
-             text=gdf.index)
+             text=gdf.geoid)
 
 app = dash.Dash()
 
-app.layout = dcc.Graph(
-    id="mapbox",
-    figure={
-        "data": [trace],
-        "layout":
-        dict(autosize=True,
-             hovermode="closest",
-             margin=dict(l=0, r=0, t=0, b=0),
-             mapbox=dict(accesstoken=mapbox_access_token,
-                         bearing=0,
-                         center=dict(lon=dc.tracts.unary_union.centroid.x,
-                                     lat=dc.tracts.unary_union.centroid.y),
-                         style="light",
-                         pitch=0,
-                         zoom=10,
-                         layers=layers))
-    },
-    style={"height": "100vh"})
+app.layout = html.Div([
+    html.H1(children='geosnap'),
+    dcc.Dropdown(id='overlay-choice',
+                 options=opts,
+                 value='n_nonhisp_black_persons'),
+    html.Div([
+        dcc.Graph(id='map-display', style={"height": "100vh"}),
+    ])
+])
 
-app.run_server()
+map_layout = {
+    'title':
+    'geosnap',
+    'data': [{
+        'name': 'Layer name',
+        'sourcetype': 'geojson',
+        'type': 'fill',
+        'opacity': 1.0,
+        'color': '#FFFFFF',  # Dynamically replace this with your color options
+        'type': 'scattermapbox',
+        'text': gdf['geoid'],
+        'hoverinfo': 'text',
+        'showlegend': True,
+    }],
+    'layout': {
+        'autosize': True,
+        'hovermode': 'closest',
+        'margin': {
+            'l': 0,
+            'r': 0,
+            'b': 0,
+            't': 0
+        },
+        'mapbox': {
+            'accesstoken': mapbox_access_token,
+            'center': {
+                'lat': gdf.unary_union.centroid.y,
+                'lon': gdf.unary_union.centroid.x,
+            },
+            'style': "light",
+            'zoom': 8.0,
+            'bearing': 0.0,
+            'pitch': 0.0,
+        },
+    }
+}
+
+
+@app.callback(dash.dependencies.Output('map-display', 'figure'),
+              [dash.dependencies.Input('overlay-choice', 'value')])
+def update_map(overlay_choice):
+
+    tmp = map_layout.copy()
+
+    gdf = dc.tracts[dc.tracts.geoid.str.startswith('11')]
+    gdf = gdf.merge(dc.census[dc.census.year == 2010], on='geoid')
+
+    gdf = gdf.assign(
+        cl=mapclassify.Equal_Interval(gdf[overlay_choice], k=6).yb)
+    # Create a layer for each region colored by LEP value
+
+    layers = []
+
+    for i, lyr in enumerate(precomputed_color_ranges):
+        example = {
+            'name': 'Layer name',
+            'source': json.loads(gdf[gdf.cl == i].to_json()),
+            'sourcetype': 'geojson',
+            'type': 'fill',
+            'opacity': 1.0,
+            'color': lyr  # Dynamically replace this with your color options
+        }
+        layers.append(example)
+
+    tmp['layout']['mapbox']['layers'] = layers
+
+    return tmp
+
+
+# End update_map()
+if __name__ == '__main__':
+
+    app.run_server()
