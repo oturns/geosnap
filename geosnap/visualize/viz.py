4aaa442671d546bc2d91a4cc33ca150618a0ea91
eli knaap <ek@knaaptime.com>
2019-06-17 11:57:39 -0700

    working choropleth

--- geosnap/visualize/viz.py
+++ geosnap/visualize/viz.py
@@ -1,25 +1,42 @@
 # first you have to load the geojson file
 import os
 import dash
+import geopandas as gpd
 import dash_core_components as dcc
-
+import mapclassify
+import palettable
 import json
 from geosnap.data import Community
 
 dc = Community(statefips='11', source='ltdb')
 
-dirname = os.path.abspath(os.path.dirname(__file__))
-jsonpath = os.path.join(dirname, 'data.geojson')
 mapbox_access_token = 'pk.eyJ1Ijoia25hYXB0aW1lIiwiYSI6ImlQeUJxazgifQ.35yYbOewGVVf7OkcM64obQ'
 
-print(jsonpath)
+gdf = dc.tracts[dc.tracts.geoid.str.startswith('11')]
+gdf = gdf.merge(dc.census[dc.census.year == 2010], on='geoid')
 
-with open(jsonpath) as geofile:
-    geojson_layer = json.load(geofile)
+gdf = gdf.assign(
+    cl=mapclassify.Equal_Interval(gdf.n_nonhisp_black_persons, k=6).yb)
+
+precomputed_color_ranges = palettable.colorbrewer.sequential.Blues_6.hex_colors
+layers = []
+
+for i, lyr in enumerate(precomputed_color_ranges):
+    example = {
+        'name': 'Layer name',
+        'source': json.loads(gdf[gdf.cl == i].to_json()),
+        'sourcetype': 'geojson',
+        'type': 'fill',
+        'opacity': 1.0,
+        'color': lyr  # Dynamically replace this with your color options
+    }
+    layers.append(example)
 
 trace = dict(type='scattermapbox',
              autocolorscale=True,
-             locations=dc.tracts['geoid'])
+             locations=dc.tracts['geoid'],
+             name='DC',
+             text=gdf.index)
 
 app = dash.Dash()
 
@@ -37,15 +54,8 @@ app.layout = dcc.Graph(
                                      lat=dc.tracts.unary_union.centroid.y),
                          style="light",
                          pitch=0,
-                         zoom=6,
-                         layers=[
-                             dict(
-                                 type="fill",
-                                 sourcetype="geojson",
-                                 source=geojson_layer,
-                                 opacity=0.8,
-                             )
-                         ]))
+                         zoom=10,
+                         layers=layers))
     },
     style={"height": "100vh"})
 
