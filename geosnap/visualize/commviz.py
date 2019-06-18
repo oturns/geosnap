56d014c96a563ab6f26df91963583cdaf34d729c
eli knaap <ek@knaaptime.com>
2019-06-18 09:31:27 -0700

    formatting

---
+++ geosnap/visualize/commviz.py
@@ -0,0 +1,175 @@
+# first you have to load the geojson file
+import os
+import dash
+import geopandas as gpd
+import pandas as pd
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_bootstrap_components as dbc
+
+import mapclassify
+import palettable
+import json
+from geosnap.data import dictionary
+from geosnap.data import metros, Community
+
+dictionary.variable.tolist()
+mapbox_access_token = 'pk.eyJ1Ijoia25hYXB0aW1lIiwiYSI6ImlQeUJxazgifQ.35yYbOewGVVf7OkcM64obQ'
+external_stylesheets = [dbc.themes.BOOTSTRAP]
+
+#gdf = dc.tracts[dc.tracts.geoid.str.startswith('11')]
+
+opts = [{'label': col.title(), 'value': col} for col in dictionary.variable]
+metros = metros.sort_values(by='name')
+
+metro_opts = [{
+    'label': str(metro['name']),
+    'value': metro['geoid']
+} for _, metro in metros.iterrows()]
+
+precomputed_color_ranges = palettable.colorbrewer.sequential.Blues_6.hex_colors
+layers = []
+
+trace = dict(type='scattermapbox', autocolorscale=True, name='metro')
+
+body = dbc.Container(
+    [
+        html.H2(children='geosnap variable explorer',
+                style={'textAlign': 'center'}),
+        dbc.Row([
+            dbc.Col(
+                [
+                    html.H5(children='Metropolitan Region',
+                            style={
+                                "padding-top": "2%",
+                                "padding-bottom": "2%"
+                            }),
+                    dcc.Dropdown(
+                        id='metro-choice', options=metro_opts, value='41740'),
+                    html.H5(children='Variable',
+                            style={
+                                "padding-top": "2%",
+                                "padding-bottom": "2%"
+                            }),
+                    dcc.Dropdown(id='overlay-choice',
+                                 options=opts,
+                                 value='median_home_value'),
+                    html.H5(children='Year',
+                            style={
+                                "padding-top": "2%",
+                                "padding-bottom": "2%"
+                            }),
+                    html.Div(dcc.Slider(id='year-slider',
+                                        min=1970,
+                                        max=2010,
+                                        value=2010,
+                                        marks={
+                                            str(year): str(year)
+                                            for year in range(1970, 2011, 10)
+                                        },
+                                        step=10),
+                             style={
+                                 "padding-left": "5%",
+                                 "padding-right": "5%",
+                                 "padding-top": "2%",
+                                 "padding-bottom": "4%"
+                             })
+                ],
+                md=4,
+            ),
+            dbc.Col([
+                dcc.Graph(id='map-display'),
+            ]),
+        ])
+    ],
+    className="mt-4",
+)
+
+app = dash.Dash(external_stylesheets=external_stylesheets)
+
+app.layout = html.Div([body])
+
+map_layout = {
+    'title':
+    'geosnap',
+    'data': [{
+        'name': 'Layer name',
+        'sourcetype': 'geojson',
+        'opacity': 0.8,
+        'type': 'scattermapbox',
+        'hoverinfo': 'text',
+        'showlegend': True,
+        'mode': 'markers+text',
+        'textposition': 'top'
+    }],
+    'layout': {
+        'autosize': True,
+        'hovermode': 'closest',
+        'margin': {
+            'l': 0,
+            'r': 0,
+            'b': 0,
+            't': 0
+        },
+        'mapbox': {
+            'accesstoken': mapbox_access_token,
+            'center': {
+                'lat': 0,
+                'lon': 0,
+            },
+            'style': "light",
+            'zoom': 8,
+            'bearing': 0.0,
+            'pitch': 0.0,
+        },
+    }
+}
+
+
+@app.callback(dash.dependencies.Output('map-display', 'figure'), [
+    dash.dependencies.Input('overlay-choice', 'value'),
+    dash.dependencies.Input('metro-choice', 'value'),
+    dash.dependencies.Input('year-slider', 'value')
+])
+def update_map(overlay_choice, metro_choice, year_choice):
+
+    tmp = map_layout.copy()
+
+    community = Community(source='ltdb', cbsafips=metro_choice)
+
+    gdf = community.tracts.merge(
+        community.census[community.census.year == year_choice], on='geoid')
+
+    gdf = gdf.assign(
+        cl=mapclassify.Equal_Interval(gdf[overlay_choice], k=6).yb)
+    # Create a layer for each region colored by LEP value
+
+    gdf = gdf[['geoid', 'cl', 'geometry']]
+
+    layers = []
+
+    for i, lyr in enumerate(precomputed_color_ranges):
+        example = {
+            'name': 'Layer name',
+            'source': json.loads(gdf[gdf.cl == i].to_json()),
+            'sourcetype': 'geojson',
+            'type': 'fill',
+            'opacity': 0.8,
+            'color': lyr
+        }
+        layers.append(example)
+
+    tmp['layout']['mapbox']['layers'] = layers
+    tmp['layout']['mapbox']['center'] = {
+        'lat': gdf.unary_union.centroid.y,
+        'lon': gdf.unary_union.centroid.x
+    }
+    tmp['data'][0]['text'] = gdf.geoid
+    tmp['data'][0]['hovertext'] = gdf.geoid
+
+    return tmp
+
+
+if __name__ == '__main__':
+
+    app.run_server()
--- geosnap/visualize/viz.py
+++ geosnap/visualize/viz.py
@@ -2,46 +2,53 @@
 import os
 import dash
 import geopandas as gpd
+import pandas as pd
 import dash_core_components as dcc
 import dash_html_components as html
-
+from dash.dependencies import Input, Output
 import mapclassify
 import palettable
 import json
 from geosnap.data import Community
+from geosnap.data import metros
+from geosnap.util import convert_gdf
 
 dc = Community(statefips='11', source='ltdb')
 
 mapbox_access_token = 'pk.eyJ1Ijoia25hYXB0aW1lIiwiYSI6ImlQeUJxazgifQ.35yYbOewGVVf7OkcM64obQ'
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-gdf = dc.tracts[dc.tracts.geoid.str.startswith('11')]
-#gdf = gdf.merge(dc.census[dc.census.year == 2010], on='geoid')
+#gdf = dc.tracts[dc.tracts.geoid.str.startswith('11')]
 
 opts = [{'label': col.title(), 'value': col} for col in dc.census.columns]
+metros = metros.sort_values(by='name')
+
+metro_opts = [{
+    'label': str(metro['name']),
+    'value': metro['geoid']
+} for _, metro in metros.iterrows()]
 
 precomputed_color_ranges = palettable.colorbrewer.sequential.Blues_6.hex_colors
 layers = []
 
-trace = dict(type='scattermapbox',
-             autocolorscale=True,
-             locations=dc.tracts['geoid'],
-             name='DC',
-             text=gdf.geoid)
+trace = dict(type='scattermapbox', autocolorscale=True, name='metro')
 
 app = dash.Dash(external_stylesheets=external_stylesheets)
+app.config['suppress_callback_exceptions'] = True
 
 app.layout = html.Div([
-    html.H1(children='geosnap variable explorer',
+    html.H2(children='geosnap variable explorer',
             style={'textAlign': 'center'}),
+    dcc.Dropdown(id='metro-choice', options=metro_opts, value='47900'),
     dcc.Dropdown(id='overlay-choice',
                  options=opts,
                  value='n_nonhisp_black_persons'),
     html.Div([
         dcc.Graph(id='map-display', style={"height": "75vh"}),
+        html.Div(id='intermediate-value', style={'display': 'none'}),
         html.Div(dcc.Slider(id='year-slider',
                             min=dc.census['year'].min(),
-                            max=2010,
+                            max=dc.census['year'].max(),
                             value=2010,
                             marks={
                                 str(year): str(year)
@@ -57,16 +64,13 @@ app.layout = html.Div([
 ])
 
 map_layout = {
-    'title':
-    'geosnap',
     'data': [{
         'name': 'Layer name',
         'sourcetype': 'geojson',
         'type': 'fill',
-        'opacity': 1.0,
+        'opacity': 0.8,
         'color': '#FFFFFF',  # Dynamically replace this with your color options
         'type': 'scattermapbox',
-        'text': gdf['geoid'],
         'hoverinfo': 'text',
         'showlegend': True,
     }],
@@ -82,11 +86,11 @@ map_layout = {
         'mapbox': {
             'accesstoken': mapbox_access_token,
             'center': {
-                'lat': gdf.unary_union.centroid.y,
-                'lon': gdf.unary_union.centroid.x,
+                'lat': dc.tracts.unary_union.centroid.y,
+                'lon': dc.tracts.unary_union.centroid.x,
             },
             'style': "light",
-            'zoom': 10,
+            'zoom': 8,
             'bearing': 0.0,
             'pitch': 0.0,
         },
@@ -94,20 +98,32 @@ map_layout = {
 }
 
 
-@app.callback(dash.dependencies.Output('map-display', 'figure'), [
+@app.callback(dash.dependencies.Output('intermediate-value', 'children'), [
+    dash.dependencies.Input('metro-choice', 'value'),
+])
+def update_map(metro_choice):
+
+    community = Community(source='ltdb', cbsafips=metro_choice)
+
+    gdf = community.tracts.merge(community.census, on='geoid', how='right')
+    return gdf.to_json()
+
+
+@app.callback(Output('map-display', 'figure'), [
+    dash.dependencies.Input('intermediate-value', 'children'),
     dash.dependencies.Input('overlay-choice', 'value'),
     dash.dependencies.Input('year-slider', 'value')
 ])
-def update_map(overlay_choice, year_choice):
+def update_graph_1(jsonified_data, overlay_choice, year_choice):
 
+    gdf = gpd.read_file(jsonified_data)
     tmp = map_layout.copy()
-
-    gdf = dc.tracts[dc.tracts.geoid.str.startswith('11')]
-    gdf = gdf.merge(dc.census[dc.census.year == year_choice], on='geoid')
-
+    gdf[gdf['year'] == year_choice]
     gdf = gdf.assign(
         cl=mapclassify.Equal_Interval(gdf[overlay_choice], k=6).yb)
     # Create a layer for each region colored by LEP value
+
+    gdf = gdf[['geoid', 'cl', 'geometry']]
 
     layers = []
 
@@ -117,17 +133,19 @@ def update_map(overlay_choice, year_choice):
             'source': json.loads(gdf[gdf.cl == i].to_json()),
             'sourcetype': 'geojson',
             'type': 'fill',
-            'opacity': 1.0,
-            'color': lyr  # Dynamically replace this with your color options
+            'opacity': 0.8,
+            'color': lyr
         }
         layers.append(example)
 
     tmp['layout']['mapbox']['layers'] = layers
+    tmp['layout']['mapbox']['center'] = {
+        'lat': gdf.unary_union.centroid.y,
+        'lon': gdf.unary_union.centroid.x
+    }
 
     return tmp
 
 
-# End update_map()
 if __name__ == '__main__':
-
     app.run_server()
