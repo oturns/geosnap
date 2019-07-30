1ff26aab12743e6d4a6d4ec21127e5cef698c1e1
eli knaap <ek@knaaptime.com>
2019-07-30 16:40:23 -0700

    fix test for 2010 tracts

--- geosnap/tests/test_data_boundaries.py
+++ geosnap/tests/test_data_boundaries.py
@@ -11,4 +11,4 @@ def test_tracts():
 
     assert data.data_store.tracts_1990(convert=False).shape[0] == 60658
     assert data.data_store.tracts_2000(convert=False).shape == (65506, 192)
-    assert data.data_store.tracts_2010(convert=False).shape == (73056, 194)
+    assert data.data_store.tracts_2010(convert=False).shape == (72832, 194)
