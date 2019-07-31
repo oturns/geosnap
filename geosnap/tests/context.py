03897c4fcc9db358d3b35f30a15a1412c0a24090
eli knaap <ek@knaaptime.com>
2019-07-30 17:05:10 -0700

    prioritize conda-forge in environment.yml

--- geosnap/tests/context.py
+++ geosnap/tests/context.py
@@ -7,17 +7,3 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")
 
 import analyze
 import data
-
-
-def quilt_check():
-
-    # get census data from spatialucr quilt account
-    try:  # if any of these aren't found, the user needs to refresh the quilt data package
-        from quilt3.data.census import tracts_cartographic, administrative
-    except AttributeError:
-        warn(
-            "Quilt data is outdated... rebuilding\n"
-            " You will need to restart your Python kernel once downloading has completed"
-        )
-        quilt3.Package.install("census/tracts_cartographic", "s3://quilt-cgs")
-        quilt3.Package.install("census/administrative", "s3://quilt-cgs")
