c25e0dda21e3c3090ca96ae269ace7f7dbb4580f
weikang9009 <weikang9009@gmail.com>
2019-08-16 20:52:12 -0700

    refactor pytests for dynamics module

--- geosnap/tests/test_dynamics.py
+++ geosnap/tests/test_dynamics.py
@@ -1,89 +1,83 @@
-from context import analyze
+# from context import analyze
 import numpy as np
 import pytest
 
-Sequence = analyze.dynamics.Sequence
+from context import data
+import os
+from ..analyze import sequence, transition
 
-# def test_Sequence_unequal():
-#     '''
-#     1. Testing on sequences of unequal lengths.
-#     '''
-#
-#     seq1 = 'ACGGTAG'
-#     seq2 = 'CCTAAG'
-#     seq3 = 'CCTAAGC'
-#
-#     # 1.1 substitution cost matrix and indel cost are not given, and will be
-#     # generated based on the distance type "interval"
-#     seqAna = Sequence([seq1, seq2, seq3], dist_type="interval")
-#     subs_mat = np.array([[0., 1., 2., 3.],
-#                          [1., 0., 1., 2.],
-#                          [2., 1., 0., 1.],
-#                          [3., 2., 1., 0.]])
-#     seq_dis_mat = np.array([[ 0.,  7., 10.],
-#                             [ 7.,  0.,  3.],
-#                             [10.,  3.,  0.]])
-#     assert seqAna.k == 4
-#     assert all([a == b for a, b in zip(seqAna.subs_mat.flatten(),
-#                                        subs_mat.flatten())])
-#     assert all([a == b for a, b in zip(seqAna.seq_dis_mat.flatten(),
-#                                        seq_dis_mat.flatten())])
-#
-#
-#     # 1.2 User-defined substitution cost matrix and indel cost
-#     subs_mat = np.array([[0, 0.76, 0.29, 0.05],
-#                          [0.30, 0, 0.40, 0.60],
-#                          [0.16, 0.61, 0, 0.26],
-#                          [0.38, 0.20, 0.12, 0]])
-#     indel = subs_mat.max()
-#     seqAna = Sequence([seq1, seq2, seq3], subs_mat=subs_mat, indel=indel)
-#     seq_dis_mat = np.array([[0.  , 1.94, 2.46],
-#                             [1.94, 0.  , 0.76],
-#                             [2.46, 0.76, 0.  ]])
-#     assert all([a == b for a, b in zip(seqAna.seq_dis_mat.flatten(),
-#                                        seq_dis_mat.flatten())])
-#
-#     # 1.3 Calculating "hamming" distance will fail on unequal sequences
-#
-#     with pytest.raises(ValueError,):
-#         Sequence([seq1, seq2, seq3], dist_type="hamming")
-#
-#
-# def test_Sequence_equal():
-#     '''
-#     2. Testing on sequences of equal length.
-#     '''
-#
-#     seq1 = 'ACGGTAG'
-#     seq2 = 'CCTAAGA'
-#     seq3 = 'CCTAAGC'
-#
-#     # 2.1 Calculating "hamming" distance will not fail on equal sequences
-#
-#     seqAna = Sequence([seq1, seq2, seq3], dist_type="hamming")
-#     seq_dis_mat = np.array([[0., 6., 6.],
-#                             [6., 0., 1.],
-#                             [6., 1., 0.]])
-#     assert all([a == b for a, b in zip(seqAna.seq_dis_mat.flatten(),
-#                                        seq_dis_mat.flatten())])
-#
-#     # 2.2 User-defined substitution cost matrix and indel cost (distance
-#     #     between different types is always 1 and indel cost is 2 or larger) -
-#     #     give the same sequence distance matrix as "hamming" distance
-#     subs_mat = np.array([[0., 1., 1., 1.], [1., 0., 1., 1.], [1., 1., 0., 1.],
-#                          [1., 1., 1., 0.]])
-#     indel = 2
-#     seqAna = Sequence([seq1, seq2, seq3], subs_mat=subs_mat, indel=indel)
-#     seq_dis_mat = np.array([[0., 6., 6.],[6., 0., 1.],[6., 1., 0.]])
-#     assert all([a == b for a, b in zip(seqAna.seq_dis_mat.flatten(),
-#                                        seq_dis_mat.flatten())])
-#
-#     # 2.3 User-defined substitution cost matrix and indel cost (distance
-#     #     between different types is always 1 and indel cost is 1) - give a
-#     #     slightly different sequence distance matrix from "hamming" distance since
-#     #     insertion and deletion is happening
-#     indel = 1
-#     seqAna = Sequence([seq1, seq2, seq3], subs_mat=subs_mat, indel=indel)
-#     seq_dis_mat = np.array([[0., 5., 5.],[5., 0., 1.],[5., 1., 0.]])
-#     assert all([a == b for a, b in zip(seqAna.seq_dis_mat.flatten(),
-#                                        seq_dis_mat.flatten())])
+path = os.environ["DLPATH"]
+
+if not os.path.exists(
+    os.path.join(os.path.dirname(os.path.abspath(data.__file__)), "ltdb.parquet")
+):
+    data.store_ltdb(sample=path + "/ltdb_sample.zip", fullcount=path + "/ltdb_full.zip")
+
+columbus = data.Community.from_ltdb(msa_fips="18140")
+columns = ['median_household_income','p_poverty_rate',
+           'p_edu_college_greater', 'p_unemployment_rate']
+columbus1 = columbus.cluster(columns=['median_household_income',
+                                      'p_poverty_rate',
+                                      'p_edu_college_greater',
+                                      'p_unemployment_rate'], method='ward')
+
+def test_transition():
+    '''
+    Testing transition modeling.
+    '''
+
+
+    # 1. Markov modeling
+    m = transition(columbus1.gdf, columbus, cluster_col="ward")
+    mp = np.array([[0.79189189, 0.00540541, 0.0027027 , 0.13243243, 0.06216216,
+        0.00540541],
+       [0.0203252 , 0.75609756, 0.10569106, 0.11382114, 0.        ,
+        0.00406504],
+       [0.00917431, 0.20183486, 0.75229358, 0.01834862, 0.        ,
+        0.01834862],
+       [0.1959799 , 0.18341709, 0.00251256, 0.61809045, 0.        ,
+        0.        ],
+       [0.32307692, 0.        , 0.        , 0.        , 0.66153846,
+        0.01538462],
+       [0.09375   , 0.0625    , 0.        , 0.        , 0.        ,
+        0.84375   ]])
+    assert all([a == b for a, b in zip(m.p.flatten(),
+                                       mp.flatten())])
+
+    # 2. Spatial Markov modeling
+    sm = transition(columbus1.gdf, columbus, cluster_col="ward", w_type="queen")
+    smp = np.array([[0.82068966, 0.        , 0.        , 0.10689655, 0.07241379,
+         0.        ],
+        [0.14285714, 0.57142857, 0.14285714, 0.14285714, 0.        ,
+         0.        ],
+        [0.5       , 0.        , 0.5       , 0.        , 0.        ,
+         0.        ],
+        [0.21428571, 0.0952381 , 0.        , 0.69047619, 0.        ,
+         0.        ],
+        [0.22222222, 0.        , 0.        , 0.        , 0.75      ,
+         0.02777778],
+        [0.16666667, 0.        , 0.        , 0.        , 0.        ,
+         0.83333333]])
+    assert all([a == b for a, b in zip(sm.P[0].flatten(),
+                                       smp.flatten())])
+
+def test_sequence():
+    '''
+    Testing sequence modeling.
+    '''
+
+    # 1. Transition-orientied optimal matching
+    gdf_new, df_wide, seq_dis_mat = sequence(columbus1.gdf, seq_clusters=5,
+                                             dist_type="tran",
+                                             cluster_col="ward")
+
+    values = np.array([3, 3, 0, 2, 3, 2])
+    assert all([a == b for a, b in zip(values, df_wide.values[0])])
+
+    # 2. Hamming distance
+
+    gdf_new, df_wide, seq_dis_mat = sequence(columbus1.gdf, seq_clusters=5,
+                                             dist_type="hamming",
+                                             cluster_col="ward")
+    values = np.array([3, 3, 0, 2, 3, 4])
+    assert all([a == b for a, b in zip(values, df_wide.values[0])])
