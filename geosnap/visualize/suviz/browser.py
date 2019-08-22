5bf5ee5ffb64b9d816801b4e82e1e117c2dcfc4d
Serge Rey <sjsrey@gmail.com>
2019-08-21 20:58:07 -0700

    ENH: adding template example

--- geosnap/visualize/suviz/browser.py
+++ geosnap/visualize/suviz/browser.py
@@ -1,3 +1,18 @@
+import urllib.parse
 import webbrowser
-url = 'http://sarasen.asuscomm.com/GEOSNAP_CL/2_Bivariate_ACM/Bivariate_ACM.html'
+import os
+
+# uncomment to local a remote file in a browser window
+#url = 'http://sarasen.asuscomm.com/GEOSNAP_CL/2_Bivariate_ACM/Bivariate_ACM.html'
+#webbrowser.open(url)
+
+# openning a local file
+
+local_dir = os.path.dirname(os.path.realpath(__file__))
+print(local_dir)
+fname =urllib.parse.quote('Adaptive Choropleth Mapper with Parallel Coordinates Plot.html')
+template_dir = os.path.join(local_dir, 'templates')
+url = 'file://' + os.path.join(template_dir, fname)
+print(url)
 webbrowser.open(url)
+
