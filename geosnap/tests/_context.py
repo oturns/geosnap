6a289c06446ac00625169aa2e06310bcb792b8ec
eli knaap <ek@knaaptime.com>
2019-11-07 10:10:32 -0800

    rm context file

--- geosnap/tests/_context.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
-from geosnap import Community, datasets
-import analyze
-import io
-import harmonize
