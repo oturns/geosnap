21ef0b2f726c830cfb33ea2df53521e2bbb86fee
eli knaap <ek@knaaptime.com>
2020-04-07 12:41:08 -0700

    better handling local quilt packages

--- geosnap/tests/test_add_data.py
+++ geosnap/tests/test_add_data.py
@@ -1,18 +1,24 @@
 import os
-
+import pytest 
 from geosnap import datasets, io
 
 path = os.environ["DLPATH"]
+try:
+    LTDB = os.environ["LTDB_SAMPLE"]
+    NCDB = os.environ["NCDB"]
+except:
+    LTDB=None
+    NCDB=None
 store_ltdb = io.store_ltdb
 store_ncdb = io.store_ncdb
 
-
+@pytest.mark.skipif(not LTDB, reason="unable to locate LTDB data")
 def test_store_ltdb():
 
     store_ltdb(sample=path + "/ltdb_sample.zip", fullcount=path + "/ltdb_full.zip")
     assert datasets.ltdb().shape == (330388, 192)
 
-
+@pytest.mark.skipif(not NCDB, reason="unable to locate Geolytics data")
 def test_store_ncdb():
 
     store_ncdb(path + "/ncdb.csv")
