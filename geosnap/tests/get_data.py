7ed7ec60db2de5741c9853c80423286311c3ae67
eli knaap <ek@knaaptime.com>
2020-04-07 17:43:55 -0700

    install ncdb at start also

--- geosnap/tests/get_data.py
+++ geosnap/tests/get_data.py
@@ -5,5 +5,7 @@ path = os.environ['DLPATH'];
 
 try:
     io.store_ltdb(sample=path + '/ltdb_sample.zip', fullcount=path + '/ltdb_full.zip')
+    io.store_ncdb(path + "/ncdb.csv")
+
 except:
     pass
