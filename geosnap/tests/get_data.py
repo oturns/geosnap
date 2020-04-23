b8305c16f7f19b4de88567beafd8c0eff2f237fe
eli knaap <ek@knaaptime.com>
2020-04-23 16:38:40 -0700

    patthlib for ncdb

--- geosnap/tests/get_data.py
+++ geosnap/tests/get_data.py
@@ -8,8 +8,8 @@ except:
 path = os.getcwd()
 
 try:
-    io.store_ltdb(sample=PurePath(path,'ltdb_sample.zip'), fullcount=PurePath(path,'ltdb_full.zip'))
-    io.store_ncdb(path + "ncdb.csv")
+    io.store_ltdb(sample=PurePath(path, 'ltdb_sample.zip'), fullcount=PurePath(path, 'ltdb_full.zip'))
+    io.store_ncdb(PurePath(path, "ncdb.csv"))
 
 except:
     pass
