4c8460b632883c85ad532c437ce229790b573556
eli knaap <ek@knaaptime.com>
2020-04-23 12:33:33 -0700

    dont use dlpath variable

--- geosnap/tests/get_data.py
+++ geosnap/tests/get_data.py
@@ -5,10 +5,9 @@ try:
 except:
     pass
 
-path = os.environ['DLPATH']
 
 try:
-    io.store_ltdb(sample=path + '/ltdb_sample.zip', fullcount=path + '/ltdb_full.zip')
+    io.store_ltdb(sample='/ltdb_sample.zip', fullcount='/ltdb_full.zip')
     io.store_ncdb(path + "/ncdb.csv")
 
 except:
