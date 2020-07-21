2735b637d325085be6e023ef114e59ead43264d8
AnGWar-26 <andrew.warfield@protonmail.com>
2020-07-21 15:31:07 -0700

    tsplot: expose legend_kwds

--- geosnap/visualize/tsplot.py
+++ geosnap/visualize/tsplot.py
@@ -31,24 +31,29 @@ save_fig     : boolean, optional
 dpi          : int, optional
                dpi of the saved image if save_fig=True
                default is 500
+legend_kwds  : dictionary, optional
+               parameters for the legend
+               Default is 1 column on the bottom of the graph.
 """
 
 
 def tsplot(community, column, title='',
            years=[], scheme='quantiles',
            k=5,save_fig=False, dpi=500,
-           **kwargs):
+           legend_kwds='default', **kwargs):
+    if legend_kwds == 'default':
+        legend_kwds = {'ncols': 1, "loc": "b"}
     if not years:
         f, axs = plot.subplots(ncols=len(community.gdf.year.unique()))
         for i, year in enumerate(sorted(community.gdf.year.unique())):  # sort to prevent graphing out of order
             community.gdf[community.gdf.year == year].plot(column=column, ax=axs[i], scheme=scheme, k=k, **kwargs,
-                                                           legend=True, legend_kwds={'ncols': 1, "loc": "b"})
+                                                           legend=True, legend_kwds=legend_kwds)
             axs[i].format(title=year)
     else:
         f, axs = plot.subplots(ncols=len(years))
         for i, year in enumerate(years):  # display in whatever order list is passed in
             community.gdf[community.gdf.year == year].plot(column=column, ax=axs[i], scheme=scheme, k=k, **kwargs,
-                                                           legend=True, legend_kwds={'ncols': 1, "loc": "b"})
+                                                           legend=True, legend_kwds=legend_kwds)
             axs[i].format(title=year)
     if not title:  # only use title when passed
         axs.format(suptitle=column)
