da6090346af64ca9c0e8ac1b1e52e582e1d2558c
AnGWar-26 <andrew.warfield@protonmail.com>
2020-07-22 17:53:48 -0700

    tsplot: implement use of contextily - default is to use contextily with OpenStreetMap.Mapnik
    TODO: Some map providers, like Stamen, have a large attribution string and they cover a significant portion of the map. To rectify this it may be desirable to give the user control of figure size or to allow them control nrows.

--- geosnap/visualize/tsplot.py
+++ geosnap/visualize/tsplot.py
@@ -3,6 +3,7 @@ Visualization method for time series plots.
 """
 
 import proplot as plot
+import contextily as ctx
 
 __all__ = ["tsplot"]
 
@@ -34,27 +35,40 @@ dpi          : int, optional
 legend_kwds  : dictionary, optional
                parameters for the legend
                Default is 1 column on the bottom of the graph.
+ctxmap       : contextily map provider, optional
+               contextily basemap. Set to False for no basemap.
+               Default is Stamen.TonerLite       
 """
 
 
 def tsplot(community, column, title='',
            years=[], scheme='quantiles',
            k=5,save_fig=False, dpi=500,
-           legend_kwds='default', **kwargs):
+           legend_kwds='default',
+           ctxmap=ctx.providers.OpenStreetMap.Mapnik,
+           **kwargs):
     if legend_kwds == 'default':
         legend_kwds = {'ncols': 1, "loc": "b"}
+    if ctxmap:  # need to convert crs to mercator before graphing
+        community.gdf = community.gdf.to_crs(epsg=3857)
     if not years:
         f, axs = plot.subplots(ncols=len(community.gdf.year.unique()))
         for i, year in enumerate(sorted(community.gdf.year.unique())):  # sort to prevent graphing out of order
             community.gdf[community.gdf.year == year].plot(column=column, ax=axs[i], scheme=scheme, k=k, **kwargs,
                                                            legend=True, legend_kwds=legend_kwds)
+            if ctxmap:  # need set basemap of each graph
+                ctx.add_basemap(axs[i], source=ctxmap)
             axs[i].format(title=year)
     else:
+
         f, axs = plot.subplots(ncols=len(years))
         for i, year in enumerate(years):  # display in whatever order list is passed in
             community.gdf[community.gdf.year == year].plot(column=column, ax=axs[i], scheme=scheme, k=k, **kwargs,
                                                            legend=True, legend_kwds=legend_kwds)
+            if ctxmap:  # need set basemap of each graph
+                ctx.add_basemap(axs[i], source=ctxmap)
             axs[i].format(title=year)
+
     if not title:  # only use title when passed
         axs.format(suptitle=column)
     else:
