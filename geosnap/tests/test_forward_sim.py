415d3a5fc26db3f99de51284ac6533febd58ebf9
eli knaap <ek@knaaptime.com>
2020-09-08 15:30:25 -0700

    use raw census for simulation

--- geosnap/tests/test_forward_sim.py
+++ geosnap/tests/test_forward_sim.py
@@ -4,9 +4,9 @@ np.seterr(divide='ignore', invalid='ignore')
 
 
 columns = ["median_household_income", "p_poverty_rate", "p_unemployment_rate"]
-reno = Community.from_ltdb(msa_fips="39900")
+reno = Community.from_census(msa_fips="39900")
+reno = reno.harmonize(intensive_variables=columns, target_year=2010, allocate_total=True, extensive_variables=['n_total_pop'])
 reno = reno.cluster(columns=columns, method='kmeans', k=3)
-
 
 def test_single_simulation():
     simulated = reno.simulate(model_name='kmeans', base_year=2010, time_steps=1)
