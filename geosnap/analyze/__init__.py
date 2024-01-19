a3e5577763244a3ea8fcb048bdd5da74d60deb99
eli knaap <ek@knaaptime.com>
2024-01-18 17:13:26 -0800

    update test workflow

--- geosnap/analyze/__init__.py
+++ geosnap/analyze/__init__.py
@@ -1,9 +1,9 @@
 from . import segdyn
 from .dynamics import (
+    draw_sequence_from_gdf,
     predict_markov_labels,
     sequence,
     transition,
-    draw_sequence_from_gdf,
 )
 from .geodemo import ModelResults, cluster, find_k, find_region_k, regionalize
 from .incs import linc, lincs_from_gdf
