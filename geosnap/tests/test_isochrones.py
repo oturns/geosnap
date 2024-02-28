16dd1190383609d9981a83c33bdb3c5ea65b7fd1
eli knaap <ek@knaaptime.com>
2024-02-28 12:52:01 -0800

    move data inside function

--- geosnap/tests/test_isochrones.py
+++ geosnap/tests/test_isochrones.py
@@ -10,24 +10,27 @@ import geopandas as gpd
 import os
 import pytest
 
-if not os.path.exists("./41740.h5"):
-    import quilt3 as q3
+def get_data():
+    if not os.path.exists("./41740.h5"):
+        import quilt3 as q3
 
-    b = q3.Bucket("s3://spatial-ucr")
-    b.fetch("osm/metro_networks_8k/41740.h5", "./41740.h5")
+        b = q3.Bucket("s3://spatial-ucr")
+        b.fetch("osm/metro_networks_8k/41740.h5", "./41740.h5")
+    sd_network = pdna.Network.from_hdf5("41740.h5")
+    example_origin = 1985327805
+    
+    return example_origin, sd_network
 
-datasets = DataStore()
-sd_tracts = get_acs(datasets, county_fips="06073", years=[2018])
-sd_network = pdna.Network.from_hdf5("41740.h5")
-example_origin = 1985327805
 
 @pytest.mark.xdist_group(name="group1")
 def test_isos_from_ids():
+    example_origin, sd_network = get_data()
     iso = isochrones_from_id(example_origin, sd_network, threshold=1600)
     assert iso.area.round(6).tolist()[0] == 0.000128
 
 @pytest.mark.xdist_group(name="group1")
 def test_isos_from_gdf():
+    example_origin, sd_network = get_data()
     sd_network.nodes_df["geometry"] = gpd.points_from_xy(
         sd_network.nodes_df.x, sd_network.nodes_df.y
     )
