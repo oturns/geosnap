dc838800be58889c5d157daeaa99b4aa65cdb8ff
eli knaap <ek@knaaptime.com>
2024-02-28 12:45:01 -0800

    xdist mark to isochrone tests

--- geosnap/tests/test_isochrones.py
+++ geosnap/tests/test_isochrones.py
@@ -8,6 +8,7 @@ from geosnap import DataStore
 import pandana as pdna
 import geopandas as gpd
 import os
+import pytest
 
 if not os.path.exists("./41740.h5"):
     import quilt3 as q3
@@ -20,12 +21,12 @@ sd_tracts = get_acs(datasets, county_fips="06073", years=[2018])
 sd_network = pdna.Network.from_hdf5("41740.h5")
 example_origin = 1985327805
 
-
+@pytest.mark.xdist_group(name="group1")
 def test_isos_from_ids():
     iso = isochrones_from_id(example_origin, sd_network, threshold=1600)
     assert iso.area.round(6).tolist()[0] == 0.000128
 
-
+@pytest.mark.xdist_group(name="group1")
 def test_isos_from_gdf():
     sd_network.nodes_df["geometry"] = gpd.points_from_xy(
         sd_network.nodes_df.x, sd_network.nodes_df.y
